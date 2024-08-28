e0ade6006b89099b57c65b3a8c39d76217f46ad9
Okancan Balcı <okancan.balci@gmail.com>
2024-08-29 00:56:54 +0300

    replace matplotlib with plotly

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import pandas as pd
 import streamlit as st
 import matplotlib.pyplot as plt
 import seaborn as sns
+import plotly.express as px
+import plotly.graph_objects as go
 from sktime.forecasting.arima import AutoARIMA
 from sktime.utils.plotting import plot_series
 from sktime.split import temporal_train_test_split
@@ -14,31 +16,48 @@ data = pd.read_csv("./garan_stock.csv", index_col="Date")
 data.index = pd.PeriodIndex(data.index, freq="D")
 data["YYYYMM"] =  pd.PeriodIndex(data.index, freq="M")
 
-monthly_open_close =  (data
- .groupby("YYYYMM")
- .agg({
-     "Open" : [("Open", "first")],
-     "Close" : [("Close", "last")]
- })
- .droplevel(0, axis = 1)
-)
+tabMonthlyReturn, tabYearlyReturn = st.tabs(["Monthly Returns", "Yearly Returns"])
+with tabMonthlyReturn:
+    monthly_open_close =  (data
+    .groupby("YYYYMM")
+    .agg({
+        "Open" : [("Open", "first")],
+        "Close" : [("Close", "last")]
+    })
+    .droplevel(0, axis = 1)
+    )
 
-monthly_open_close["Month_Return"] = (monthly_open_close['Close'] - monthly_open_close["Open"]) / monthly_open_close['Open']
-monthly_open_close["Positive_Return"] = np.where(monthly_open_close.Month_Return > 0, 1, 0)
+    monthly_open_close["Month_Return"] = (monthly_open_close['Close'] - monthly_open_close["Open"]) / monthly_open_close['Open']
+    monthly_open_close["Positive_Return"] = np.where(monthly_open_close.Month_Return > 0, "Positive", "Negative")
+    monthly_open_close["YearMonth"] = monthly_open_close.index.astype("str")
+    monthly_open_close["Month_Return_Perc"] = (np.round(monthly_open_close.Month_Return,3) * 100)
+    
+    figMonthlyReturns = px.bar(data_frame=monthly_open_close,
+                               x='YearMonth', y="Month_Return_Perc", color="Positive_Return",
+                               color_discrete_sequence=["green", "red"])
+    figMonthlyReturns.layout.update(showlegend=False)
+    st.plotly_chart(figMonthlyReturns)
 
-yearly_open_close = (data
- .groupby(data.index.year)
- .agg({
-     "Open" : [("Open", "first")],
-     "Close" : [("Close", "last")]
- })
- .droplevel(0, axis = 1)
-)
+with tabYearlyReturn:
+    yearly_open_close = (data
+    .groupby(data.index.year)
+    .agg({
+        "Open" : [("Open", "first")],
+        "Close" : [("Close", "last")]
+    })
+    .droplevel(0, axis = 1)
+    )
 
-yearly_open_close["Year_Return"] = (yearly_open_close['Close'] - yearly_open_close["Open"]) / yearly_open_close['Open']
-yearly_open_close["Positive_Return"] = np.where(yearly_open_close.Year_Return > 0, 1, 0)
-yearly_open_close.index.name = "YYYY"
+    yearly_open_close["Year_Return"] = (yearly_open_close['Close'] - yearly_open_close["Open"]) / yearly_open_close['Open']
+    yearly_open_close["Positive_Return"] = np.where(yearly_open_close.Year_Return > 0, "Positive", "Negative")
+    yearly_open_close["Year"] = yearly_open_close.index.astype("str")
+    yearly_open_close["Year_Return_Perc"] = (np.round(yearly_open_close.Year_Return,3) * 100)
 
+    figYearlyReturns = px.bar(data_frame=yearly_open_close,
+                              x='Year', y="Year_Return_Perc", color="Positive_Return",
+                              color_discrete_sequence=["green", "red"])
+    figYearlyReturns.layout.update(showlegend=False)
+    st.plotly_chart(figYearlyReturns)
 
 MIN_DATE = pd.to_datetime(str(data.index.min())).to_pydatetime()
 MAX_DATE = pd.to_datetime(str(data.index.max())).to_pydatetime()
@@ -53,16 +72,33 @@ with st.sidebar:
 
 full_data = data.loc[(data.index >= str(data_date[0])) & (data.index <= str(data_date[1])), data_col]
 
-# fig, ax = plot_series(training_data)
-# st.pyplot(fig)
-
+## FULL DATA MODEL
 arima = AutoARIMA(maxiter=100)
 arima.fit(full_data)
 fh = np.arange(1, 20)
 preds = arima.predict(fh)
 intervals = arima.predict_interval(fh, coverage=[0.5, 0.9])
-figARIMA, axARIMA = plot_series(full_data, preds, pred_interval=intervals)
-st.pyplot(figARIMA)
+
+conf_50 = pd.concat([intervals['Close'][0.5].upper, intervals['Close'][0.5].lower[::-1]])
+conf_90 = pd.concat([intervals['Close'][0.9].upper, intervals['Close'][0.9].lower[::-1]])
+
+fig_all_data = go.Figure()
+fig_all_data.add_trace(go.Scatter(
+    x=full_data.index.to_timestamp(), y=full_data,
+    line_color="steelblue", mode="lines+markers", name="Observations"
+))
+fig_all_data.add_trace(go.Scatter(
+    x=conf_90.index.to_timestamp(), y=conf_90, line_color="yellow",
+    fill="toself", mode="lines+markers", name="90% Confidence"
+))
+fig_all_data.add_trace(go.Scatter(
+    x=conf_50.index.to_timestamp(), y=conf_50, line_color="lightgreen",
+    fill="toself", mode="lines+markers", name="50% Confidence"
+))
+fig_all_data.add_trace(go.Scatter(
+    x=preds.index.to_timestamp(), y=preds, line_color="orange", mode="lines+markers", name="Predictions"
+))
+st.plotly_chart(fig_all_data)
 
 train_data, test_data = temporal_train_test_split(full_data, test_size=input_test_size)
 
@@ -75,19 +111,3 @@ preds.index = test_data.index
 intervals.index = test_data.index
 figARIMATT, axARIMATT = plot_series(train_data, preds, test_data, pred_interval=intervals, labels=["train", "pred", "actual"])
 st.pyplot(figARIMATT)
-
-figMonthlyReturn, axMonthlyReturn = plt.subplots()
-sns.barplot(data=monthly_open_close,
-            x="YYYYMM", y="Month_Return",hue="Positive_Return", hue_order=[1,0],
-             ax=axMonthlyReturn)
-
-figYearlyReturn, axYearlyReturn = plt.subplots()
-sns.barplot(data=yearly_open_close,
-            x="YYYY", y="Year_Return",hue="Positive_Return", hue_order=[1,0],
-             ax=axYearlyReturn)
-
-col1, col2 =  st.columns([3,2])
-with col1:
-    st.pyplot(figMonthlyReturn)
-with col2:
-    st.pyplot(figYearlyReturn)
